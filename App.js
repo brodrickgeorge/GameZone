ef2071f091c8639564f4633a3f35fc702883caa8
brodrickgeorge <brodrick.george@yahoo.com>
2021-07-19 19:36:40 -0500

    added header

--- App.js
+++ App.js
@@ -1,49 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "Brodrick", id: "1" },
-    { name: "Cynthia", id: "2" },
-    { name: "Jarod", id: "3" },
-    { name: "Derrick", id: "4" },
-    { name: "Ryan", id: "5" },
-    { name: "Nicole", id: "6" },
-    { name: "Brenden", id: "7" },
-    { name: "Bradley", id: "8" },
-    { name: "Duan", id: "9" },
-    { name: "Jennifer", id: "10" },
+  const [todos, setTodos] = useState([
+    { text: "buy coffee.", key: "1" },
+    { text: "create an app.", key: "2" },
+    { text: "play on the switch.", key: "3" },
   ]);
 
-  const handlePress = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.id != id);
-    });
-  };
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handlePress(item.id)}>
-            <Text style={styles.personName}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/*form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -51,17 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  personName: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginTop: 24,
-    marginHorizontal: 10,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+export default function Header() {
+  return (
+    <SafeAreaView style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    padding: 38,
+    backgroundColor: "coral",
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
