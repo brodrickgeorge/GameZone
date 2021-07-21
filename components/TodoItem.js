d87b9545f047fbfaa673cbe582a143838d7c07fb
brodrickgeorge <brodrick.george@yahoo.com>
2021-07-20 19:51:24 -0500

    added materialicon

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,22 +1,30 @@
 import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
 
 export default function TodoItem({ item, handlePress }) {
   return (
     <TouchableOpacity onPress={() => handlePress(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={18} color="#333" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
